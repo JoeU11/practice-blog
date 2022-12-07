c419c3c4e09a66be8cc47f1acc5c7a9871fc19c0
joe <joe.ulica@gmail.com>
2022-12-07 16:34:48 -0500

    add unstyled navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import Pen from "../images/pen.png"
+
+export default function () {
+  return (
+    <nav>
+      <img src={Pen} alt="blog-logo" id="nav-logo" />
+      <h1 id="nav-title">Blog About it!</h1>
+    </nav>
+  )
+}
