f1e8e09eb95452e31ac194788160cd7a879fd836
joe <joe.ulica@gmail.com>
2022-12-07 17:20:32 -0500

    mock up sample blog data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Navbar from "./components/Navbar"
+import BlogData from './BlogData';
 
 function App() {
+  console.log(BlogData)
   return (
     <div className="App">
       <Navbar />
